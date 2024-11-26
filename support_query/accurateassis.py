e5881c629fa0b76ab6f23e0f86c392743fd3a9ce
SonderXiaoming <1316864423@qq.com>
2024-11-25 22:31:41 -0600

    support ex equipment（only clanbattle）

--- support_query/accurateassis.py
+++ support_query/accurateassis.py
@@ -5,198 +5,270 @@ from hoshino.modules.priconne import chara
 from hoshino.modules.priconne._pcr_data import CHARA_NAME
 
 equip_dict = {
-"sliver":{0:0,1:30,2:80,3:160},#4
-"golden":{0:0,1:60,2:160,3:360,4:700,5:1200},#7
-"purple":{0:0,1:100,2:260,3:540,4:1020,5:1800},#11
-}#装备经验
+    "sliver": {0: 0, 1: 30, 2: 80, 3: 160},  # 4
+    "golden": {0: 0, 1: 60, 2: 160, 3: 360, 4: 700, 5: 1200},  # 7
+    "purple": {0: 0, 1: 100, 2: 260, 3: 540, 4: 1020, 5: 1800},  # 11
+}  # 装备经验
 
-unquie_equip=[
-(0,10),
-(90,15),
-(240,25),
-(490,40),
-(890,50),
-(1390,60),
-(1990,75),
-(2740,100)
+unquie_equip = [
+    (0, 10),
+    (90, 15),
+    (240, 25),
+    (490, 40),
+    (890, 50),
+    (1390, 60),
+    (1990, 75),
+    (2740, 100),
 ]
 
-bonus_dict ={
-"atk":"物理攻击力",
-"crt": "物理暴击率",
-"matk": "魔法攻击力",
-"mcrt": "魔法暴击率",
-"erec": "TP自动回复",
-"hp": "血量",
-"def": "物理防御力",
-"mdef": "魔法防御力",
-"hrec_rate": "回复量上升",
-"erec_rate" : "TP上升"
+ex_equip = [
+    (150, 400, 800),
+    (150, 400, 800, 1300),
+    (800, 1800, 3000, 4400, 6000),  # 后面一样了
+]
+
+bonus_dict = {
+    "atk": "物理攻击力",
+    "crt": "物理暴击率",
+    "matk": "魔法攻击力",
+    "mcrt": "魔法暴击率",
+    "erec": "TP自动回复",
+    "hp": "血量",
+    "def": "物理防御力",
+    "mdef": "魔法防御力",
+    "hrec_rate": "回复量上升",
+    "erec_rate": "TP上升",
 }
 
+
+def get_ex_equip_max_star(equipment_id: int) -> int:
+    rank = equipment_id % 1000 // 100
+    return rank + 2 if rank < 3 else 5
+
+
+def ex_equip_exp2star(exp: int, equipment_id: int) -> int:
+    if not equipment_id:
+        return 0
+    rank = equipment_id % 1000 // 100
+    rank = 2 if rank > 3 else rank - 1
+    exp_list = ex_equip[rank]
+    return next(
+        (i for i, star_exp in enumerate(exp_list, 1) if exp < star_exp),
+        len(exp_list),
+    )
+
+
 class accurateassis(object):
-    def __init__(self,assisjson_path) :
+    def __init__(self, assisjson_path):
         self.assisjson_path = assisjson_path
-        with open (self.assisjson_path,"r",encoding="utf-8") as f:
+        with open(self.assisjson_path, "r", encoding="utf-8") as f:
             self.assisjson = json.load(f)
-        
-    def translatename2id(self,name):
-        if name == '所有':
-            self.transresult_id =  -1
-            return ''
-        defen = re.sub(r'[?？，,_]', '', name)
+
+    def translatename2id(self, name):
+        if name == "所有":
+            self.transresult_id = -1
+            return ""
+        defen = re.sub(r"[?？，,_]", "", name)
         defen, unknown = chara.roster.parse_team(defen)
         if unknown:
             _, name, score = chara.guess_id(unknown)
             if score < 70 and not defen:
                 return  # 忽略无关对话
             unknown = filt_message(unknown)
-            msg = f'无法识别"{unknown}"' if score < 70 else f'无法识别"{unknown}" 您说的有{score}%可能是{name}'
-            return msg
+            return (
+                f'无法识别"{unknown}"'
+                if score < 70
+                else f'无法识别"{unknown}" 您说的有{score}%可能是{name}'
+            )
         else:
             self.transresult_id = defen
-            return ''
-    
-    def equip_exp2star(self,num,exp,rank):
+            return ""
+
+    def equip_exp2star(self, num, exp, rank):
         if num == 0:
             return "未装备"
-        elif rank <4:#没星
-            return '已装备'
-        elif 4<=rank<7:#3星
-            for i in range(0,3):
-                if equip_dict["sliver"][i] <= exp < equip_dict["sliver"][i+1]:
+        elif rank < 4:  # 没星
+            return "已装备"
+        elif 4 <= rank < 7:  # 3星
+            return next(
+                (
+                    i
+                    for i in range(3)
+                    if equip_dict["sliver"][i] <= exp < equip_dict["sliver"][i + 1]
+                ),
+                3,
+            )
+        elif 7 <= rank < 11:
+            for i in range(5):
+                if equip_dict["golden"][i] <= exp < equip_dict["golden"][i + 1]:
                     return i
-            return 3
-        elif 7<=rank<11:
-            for i in range(0,5):
-                if equip_dict["golden"][i] <= exp < equip_dict["golden"][i+1]:
+        elif rank > 11:
+            for i in range(5):
+                if equip_dict["purple"][i] <= exp < equip_dict["purple"][i + 1]:
                     return i
-        elif rank>11:
-            for i in range(0,5):
-                if equip_dict["purple"][i] <= exp < equip_dict["purple"][i+1]:
-                    return i
-        return 5 #FIXME：红装，绿装呢
-    
-    def unique_exp2level(self,num,exp):
+        return 5  # FIXME：红装，绿装呢
+
+    def unique_exp2level(self, num, exp):
         if num == 0:
             return "未装备"
-        elif exp<=unquie_equip[0][0]:#z专武初始1级
-            level = 1 + (exp/unquie_equip[0][1])
-        elif exp <= unquie_equip[-1][0]:#一开始毫无规律，打表
-            for index,stage in enumerate(unquie_equip):
+        elif exp <= unquie_equip[0][0]:  # z专武初始1级
+            level = 1 + (exp / unquie_equip[0][1])
+        elif exp <= unquie_equip[-1][0]:  # 一开始毫无规律，打表
+            for index, stage in enumerate(unquie_equip):
                 low = stage[0]
-                if low < exp <= unquie_equip[index+1][0]:
-                    level = (exp - low)/stage[1] + index*10
-        else:#后面每升10级，升一级所需经验值+25，10级为一组，等差数列
+                if low < exp <= unquie_equip[index + 1][0]:
+                    level = (exp - low) / stage[1] + index * 10
+        else:  # 后面每升10级，升一级所需经验值+25，10级为一组，等差数列
             exp -= unquie_equip[-1][0]
-            n = int((-7 + (49+4*exp/125)**(1/2))/2)#等差公式求出经验值多余多少个10级，向下取整
-            if n<=7:
-                level = 70 + 10*n + (exp-875*n-125*(n)**2)/(75+25*n)#算出多多少级
-            else:#每次需要250经验值值时不再增长
+            n = int(
+                (-7 + (49 + 4 * exp / 125) ** (1 / 2)) / 2
+            )  # 等差公式求出经验值多余多少个10级，向下取整
+            if n <= 7:
+                level = (
+                    70 + 10 * n + (exp - 875 * n - 125 * (n) ** 2) / (75 + 25 * n)
+                )  # 算出多多少级
+            else:  # 每次需要250经验值值时不再增长
                 n = 7
-                level = 70 + 10*n + (exp-875*n-125*(n)**2)/250
+                level = 70 + 10 * n + (exp - 875 * n - 125 * (n) ** 2) / 250
         return int(level)
-    
-    def letter2chinese(self,bonus_param):
+
+    def letter2chinese(self, bonus_param):
         for letter in list(bonus_param):
             if letter in bonus_dict:
-                bonus_param[bonus_dict[letter]] = bonus_param[letter] 
+                bonus_param[bonus_dict[letter]] = bonus_param[letter]
                 del bonus_param[letter]
-        return str(bonus_param).replace("\'","")[1:-1]
-    
-    def general_info(self,units): 
+        return str(bonus_param).replace("'", "")[1:-1]
+
+    def general_info(self, units):
         all_info = []
         for it in units:
             info = {}
             try:
-                info["player"] = it["owner_name"] #玩家名字
-                it = it["unit_data"]    
+                info["player"] = it["owner_name"]  # 玩家名字
+                it = it["unit_data"]
             except:
                 info["player"] = self.user_name
-            
-            id = it["id"]//100
+
+            id = it["id"] // 100
             if self.transresult_id == -1 or id in self.transresult_id:
                 equip_slots = []
                 rank = it["promotion_level"]
-                for equip_id in range(0,6):
-                    equip = it['equip_slot'][equip_id]['is_slot']
-                    equip_rank = it['equip_slot'][equip_id]['enhancement_pt']
-                    equip_star = self.equip_exp2star(equip,equip_rank,rank)
+                for equip_id in range(6):
+                    equip = it["equip_slot"][equip_id]["is_slot"]
+                    equip_rank = it["equip_slot"][equip_id]["enhancement_pt"]
+                    equip_star = self.equip_exp2star(equip, equip_rank, rank)
                     equip_slots.append(equip_star)
-                
-                star = (it["battle_rarity"],True) if it["battle_rarity"] != 0 else (it["unit_rarity"],False)
+
+                ex_equip_slots = []
+                for cb_ex_equip in range(3):
+                    try:
+                        ex_equip, ex_equip_rank = self.unit_ex_equip_dict.get(
+                            it["cb_ex_equip_slot"][cb_ex_equip]["serial_id"], (0, 0)
+                        )
+
+                    except:
+                        ex_equip = it["cb_ex_equip_slot"][cb_ex_equip][
+                            "ex_equipment_id"
+                        ]
+                        ex_equip_rank = it["cb_ex_equip_slot"][cb_ex_equip][
+                            "enhancement_pt"
+                        ]
+
+                    ex_equip_star = ex_equip_exp2star(ex_equip_rank, ex_equip)
+                    ex_equip_slots.append((ex_equip, ex_equip_star))
+
+                star = (
+                    (it["battle_rarity"], True)
+                    if it["battle_rarity"] != 0
+                    else (it["unit_rarity"], False)
+                )
 
                 try:
-                    unique_level = self.unique_exp2level(it['unique_equip_slot'][0]['is_slot'],
-                                                        it['unique_equip_slot'][0]['enhancement_pt'])
+                    unique_level = self.unique_exp2level(
+                        it["unique_equip_slot"][0]["is_slot"],
+                        it["unique_equip_slot"][0]["enhancement_pt"],
+                    )
                 except:
-                    unique_level = '未装备'
-                
-                try:
-                    info['skill'] = (str(it["union_burst"][0]["skill_level"]),
-                                str(it["main_skill"][0]["skill_level"]),
-                                str(it["main_skill"][1]["skill_level"]),
-                                str(it["ex_skill"][0]["skill_level"]))#"技能UB,1,2,Ex
-                except:
-                    info['skill'] = ('1','1','0','1')#"技能UB,1,2,Ex
+                    unique_level = "未装备"
 
                 try:
-                    info['special_attribute'] = self.letter2chinese(it['bonus_param'])
+                    info["skill"] = (
+                        str(it["union_burst"][0]["skill_level"]),
+                        str(it["main_skill"][0]["skill_level"]),
+                        str(it["main_skill"][1]["skill_level"]),
+                        str(it["ex_skill"][0]["skill_level"]),
+                    )  # "技能UB,1,2,Ex
                 except:
-                    info['special_attribute'] = f'好感等级{self.love_dict[id]}'
+                    info["skill"] = ("1", "1", "0", "1")  # "技能UB,1,2,Ex
 
-                info['unique_equip_slot'] = str(unique_level)
-                info['star'] = star#星级，是否调星
-                info['level'] = str(it["unit_level"])#等级
-                info['rank'] = str(rank)#rank
-                info["equip"] = equip_slots #武器穿戴情况
+                try:
+                    info["special_attribute"] = self.letter2chinese(it["bonus_param"])
+                except:
+                    info["special_attribute"] = f"好感等级{self.love_dict[id]}"
+
+                info["unique_equip_slot"] = str(unique_level)
+                info["ex_equip"] = ex_equip_slots
+                info["star"] = star  # 星级，是否调星
+                info["level"] = str(it["unit_level"])  # 等级
+                info["rank"] = str(rank)  # rank
+                info["equip"] = equip_slots  # 武器穿戴情况
                 info["id"] = id
                 all_info.append(info)
         return all_info
 
-            
     def serchassis(self):
         support_unit_list = self.assisjson["support_unit_list"]
         all_info = self.general_info(support_unit_list)
-        all_info.sort(key = lambda x:x["id"])
+        all_info.sort(key=lambda x: x["id"])
         return all_info
-    
-    def get_item(self,id):
+
+    def get_item(self, id):
         item_list = self.assisjson["item_list"]
         for item in item_list:
             if item["id"] == id:
                 return item["stock"]
         item_list = self.assisjson["user_equip"]
-        for item in item_list:
-            if item["id"] == id:
-                return item["stock"]        
-        return -1
-    
+        return next((item["stock"] for item in item_list if item["id"] == id), -1)
+
     def user_card(self):
-        self.love_dict = {}
         unit_list = self.assisjson["unit_list"]
         self.user_name = self.assisjson["user_info"]["user_name"]
         love_list = self.assisjson["user_chara_info"]
-        for data in love_list:
-            self.love_dict[data["chara_id"]] = data["love_level"]
+        self.love_dict = {data["chara_id"]: data["love_level"] for data in love_list}
+        self.unit_ex_equip_dict = {
+            equip["serial_id"]: (equip["ex_equipment_id"], equip["enhancement_pt"])
+            for equip in self.assisjson["user_ex_equip"]
+        }
         all_info = self.general_info(unit_list)
-        all_info.sort(key = lambda x:int(x["level"]),reverse = True)
+        all_info.sort(key=lambda x: int(x["level"]), reverse=True)
         return all_info
-    
+
     def user_info(self):
-        info =[]
         userinfo = self.assisjson
-        title = ["名称",userinfo["user_info"]["user_name"]]#名称
-        info.append(["等级",userinfo["user_info"]["team_level"]])#等级
-        info.append(["头像",CHARA_NAME[userinfo["user_info"]["favorite_unit_id"]//100][0]])#头像
-        info.append(["宝石",userinfo["user_jewel"]["free_jewel"] + userinfo["user_jewel"]["paid_jewel"]])#宝石
-        info.append(["马娜",userinfo["user_gold"]["gold_id_free"] + userinfo["user_gold"]["gold_id_pay"]])#mana
-        info.append(["地下城币",self.get_item(90002)])#地下城币
-        info.append(["竞技场币",self.get_item(90003)])#竞技场币
-        info.append(["公主竞技场币",self.get_item(90004)])#公主竞技场币
-        info.append(["母猪石", self.get_item(90005)])#母猪石
-        info.append(["会战币",self.get_item(90006)])#会战币
-        info.append(["大师币",self.get_item(90008)])#大师币
-        info.append(["星球杯",self.get_item(25001)])#星球杯
-        info.append(["心碎",self.get_item(140001)])#心碎
+        title = ["名称", userinfo["user_info"]["user_name"]]  # 名称
+        info = [
+            ["等级", userinfo["user_info"]["team_level"]],
+            [
+                "头像",
+                CHARA_NAME[userinfo["user_info"]["favorite_unit_id"] // 100][0],
+            ],
+            [
+                "宝石",
+                userinfo["user_jewel"]["free_jewel"]
+                + userinfo["user_jewel"]["paid_jewel"],
+            ],
+            [
+                "马娜",
+                userinfo["user_gold"]["gold_id_free"]
+                + userinfo["user_gold"]["gold_id_pay"],
+            ],
+            ["地下城币", self.get_item(90002)],
+            ["竞技场币", self.get_item(90003)],
+            ["公主竞技场币", self.get_item(90004)],
+            ["母猪石", self.get_item(90005)],
+            ["会战币", self.get_item(90006)],
+            ["大师币", self.get_item(90008)],
+            ["星球杯", self.get_item(25001)],
+            ["心碎", self.get_item(140001)],
+        ]
         return title, info
--- support_query/create_img.py
+++ support_query/create_img.py
@@ -1,132 +1,214 @@
+from dataclasses import dataclass
+from io import BytesIO
+import itertools
+import math
 import os
+from typing import Tuple
+
+import httpx
 from ..util.tools import FONT_PATH, RES_PATH
 from PIL import Image, ImageDraw, ImageFont
 from hoshino.modules.priconne.chara import fromid
+from .accurateassis import get_ex_equip_max_star
 
 font_cn_path = os.path.join(FONT_PATH, "SourceHanSansCN-Medium.otf")
 res_path = os.path.join(RES_PATH, "support_query")
 
-star_list_orgin = [
-Image.open(os.path.join(res_path, '16px-星星.png')).convert("RGBA"),
-Image.open(os.path.join(res_path, '16px-星星6.png')).convert("RGBA"),
-Image.open(os.path.join(res_path, '16px-星星蓝.png')).convert("RGBA"),
-Image.open(os.path.join(res_path, '16px-星星无.png')).convert("RGBA")
+IMAGE_WIDTH = 593
+IMAGE_HEIGHT = 788
+
+star_list = [
+    Image.open(os.path.join(res_path, "16px-星星.png")).convert("RGBA"),
+    Image.open(os.path.join(res_path, "16px-星星6.png")).convert("RGBA"),
+    Image.open(os.path.join(res_path, "16px-星星蓝.png")).convert("RGBA"),
+    Image.open(os.path.join(res_path, "16px-星星无.png")).convert("RGBA"),
 ]
 
-def _cut_str(obj: str, sec: int):
+ex_star_list = [
+    Image.open(os.path.join(res_path, "ex_star_white.png")).convert("RGBA"),
+    Image.open(os.path.join(res_path, "ex_star_grey.png")).convert("RGBA"),
+    Image.open(os.path.join(res_path, "ex_star_blue.png")).convert("RGBA"),
+    Image.open(os.path.join(res_path, "ex_star_red.png")).convert("RGBA"),
+]
+
+
+def get_font_size(font: ImageFont.ImageFont, text: str) -> Tuple[int, int]:
+    bbox = font.getbbox(text)
+    return bbox[2] - bbox[0], bbox[3] - bbox[1]
+
+
+def cut_str(obj: str, sec: int):
     """
     按步长分割字符串
     """
-    return [obj[i: i+sec] for i in range(0, len(obj), sec)]
+    return [obj[i : i + sec] for i in range(0, len(obj), sec)]
 
-async def draw_star(im,change,star,size,x,y):
-    star_list = star_list_orgin[:]
-    star_list[0] = star_list[0].resize((size,size))
 
+@dataclass
+class TextImageInfo:
+    w: int
+    h: int
+    base_x: int
+    base_y: int
+    text: str
+
+
+async def draw_star(
+    im: Image.Image, battle_star: int, star: int, size: int, x: int, y: int
+):
+    start1 = star_list[0].resize((size, size), Image.LANCZOS)
     if star == 6:
-        star_list[1] = star_list[1].resize((size,size))
-        for i in range(1,6):
-            im.paste(star_list[0], (x+i*size,y), mask=star_list[0])
-        im.paste(star_list[1], (x+6*size,y), mask=star_list[1])
-    elif change:
-        star_list[2] = star_list[2].resize((size,size))
-        for i in range(1,6):
-            if i <= star:
-                im.paste(star_list[0], (x+i*size,y), mask=star_list[0])
-            else:
-                im.paste(star_list[2], (x+i*size,y), mask=star_list[2])
+        start2 = start1
+        start3 = star_list[1].resize((size * 12 // 10, size * 12 // 10), Image.LANCZOS)
+        im.paste(start3, (x + 6 * size, y - size // 10), mask=start3)
+    elif battle_star:
+        start2 = star_list[2].resize((size, size), Image.LANCZOS)
     else:
-        star_list[3] = star_list[3].resize((size,size))
-        for i in range(1,6):
-            if i <= star:
-                im.paste(star_list[0], (x+i*size,y), mask=star_list[0])
-            else:
-                im.paste(star_list[3], (x+i*size,y), mask=star_list[3])
+        start2 = star_list[3].resize((size, size), Image.LANCZOS)
+        battle_star = star
 
-async def draw_img(info,font,icon_size,font_black):
-        im = Image.open(os.path.join(res_path, 'template.jpg')).convert("RGBA")
-        c_format = fromid(info["id"])
-        icon = await c_format.render_icon(icon_size)
-        im.paste(icon, (22,23), mask=icon)
+    for i in range(1, 5 + 1):
+        draw_star = start1 if i <= battle_star else start2
+        im.paste(draw_star, (x + i * size, y), mask=draw_star)
 
-        draw = ImageDraw.Draw(im)
-        w, h = font.getsize(info["player"])
-        draw.text((305-w,30-h), info["player"], font_black, font)
 
-        w, h = font.getsize(info['level'])
-        draw.text((305-w,60-h), info['level'], font_black, font)
+async def draw_ex_equip_star(
+    im: Image.Image, max_star: int, star: int, width: int, x: int, y: int
+) -> Image.Image:
+    height = width * 37 // 27
+    for i in range(1, star + 1):
+        draw_star = (
+            ex_star_list[2].resize((width, height), Image.LANCZOS)
+            if i <= 3
+            else ex_star_list[3].resize((width, height), Image.LANCZOS)
+        )
+        im.paste(draw_star, (x + i * width, y), mask=draw_star)
 
-        w, h = font.getsize(info['rank'])
-        draw.text((305-w,90-h), info['rank'], font_black, font)
+    for i in range(star + 1, max_star + 1):
+        draw_star = (
+            ex_star_list[1].resize((width, height), Image.LANCZOS)
+            if i <= 3
+            else ex_star_list[0].resize((width, height), Image.LANCZOS)
+        )
+        im.paste(draw_star, (x + i * width, y), mask=draw_star)
 
-        w, h = font.getsize(info['unique_equip_slot'])
-        draw.text((305-w,120-h), info['unique_equip_slot'], font_black, font)
 
-        w, h = font.getsize(info['skill'][0])
-        draw.text((156-w,280-h), info['skill'][0], font_black, font)
+async def get_ex_equipment_img(equipment_id, size) -> Image.Image:
 
-        w, h = font.getsize(info['skill'][3])
-        draw.text((156-w,315-h), info['skill'][3], font_black, font)
+    if not equipment_id:
+        image = Image.open(os.path.join(res_path, "unknown.png"))
 
-        w, h = font.getsize(info['skill'][1])
-        draw.text((305-w,280-h), info['skill'][1], font_black, font)
+    else:
+        path = os.path.join(res_path, "ex_equipment", f"{equipment_id}.png")
+        if os.path.exists(path):
+            image = Image.open(path)
+        else:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(
+                    f"https://pcredivewiki.tw/static/images/equipment/icon_equipment_{equipment_id}.png"
+                )
+                response.raise_for_status()
+                image = Image.open(BytesIO(response.content))
+                image.save(path)
 
-        w, h = font.getsize(info['skill'][2])
-        draw.text((305-w,315-h), info['skill'][2], font_black, font)
+    return image.resize((size, size), Image.LANCZOS).convert("RGBA")
 
-        special_attribute =  _cut_str(info['special_attribute'], 25)
 
-        text = '好感加成：'
-        text +='\n                    '.join(special_attribute)
-        draw.text((25,345),text, font_black, font)
-        
-        for index,equip in enumerate(info["equip"]):
-            if index % 2 == 0:
-                if equip == "未装备" or equip == "已装备":
-                    w, h = font.getsize(equip)
-                    draw.text((156-w,170+16*index-h), equip, font_black, font)
-                else:
-                    await draw_star(im,False,equip,12,91,160+16*index)
-            else:
-                if equip == "未装备" or equip == "已装备":
-                    w, h = font.getsize(equip)
-                    draw.text((305-w,170+16*(index-1)-h), equip, font_black, font)
-                else:
-                    await draw_star(im,False,equip,12,240,160+16*(index-1))
-        
-        await draw_star(im,info['star'][1],info['star'][0],16,6,100)
-        return im
+async def draw_img(info, font, icon_size, font_color):
+    im = Image.open(os.path.join(res_path, "pcr_unit.png")).convert("RGBA")
+    icon = (
+        (await fromid(info["id"]).get_icon(info["star"][0]))
+        .open()
+        .convert("RGBA")
+        .resize((icon_size, icon_size), Image.LANCZOS)
+    )
+    im.paste(icon, (220, 71), mask=icon)
+    await draw_star(
+        im,
+        info["star"][0] if info["star"][1] else 0,
+        5 if info["star"][1] else info["star"][0],
+        25,
+        195,
+        200,
+    )
+
+    draw = ImageDraw.Draw(im)
+    info["special_attribute"] = cut_str(info["special_attribute"], 10)
+    text = "好感加成：" + "\n                    ".join(info["special_attribute"])
+    for text_info in [
+        TextImageInfo(*get_font_size(font, info["player"]), 320, 320, info["player"]),
+        TextImageInfo(*get_font_size(font, info["level"]), 320, 368, info["level"]),
+        TextImageInfo(*get_font_size(font, info["rank"]), 320, 464, info["rank"]),
+        TextImageInfo(
+            *get_font_size(font, info["unique_equip_slot"]),
+            320,
+            416,
+            info["unique_equip_slot"],
+        ),
+        TextImageInfo(
+            *get_font_size(font, info["skill"][0]), 555, 320, info["skill"][0]
+        ),
+        TextImageInfo(
+            *get_font_size(font, info["skill"][3]), 555, 464, info["skill"][3]
+        ),
+        TextImageInfo(
+            *get_font_size(font, info["skill"][1]), 555, 368, info["skill"][1]
+        ),
+        TextImageInfo(
+            *get_font_size(font, info["skill"][2]), 555, 416, info["skill"][2]
+        ),
+        TextImageInfo(0, 0, 72, 720, text),
+    ]:
+        draw.text(
+            (text_info.base_x - text_info.w, text_info.base_y - text_info.h),
+            text_info.text,
+            font_color,
+            font,
+        )
+
+    for index, equip in enumerate(info["equip"]):
+        equip = str(equip)
+        i = index % 2
+        if equip.isdigit():
+            await draw_star(im, 0, int(equip), 18, 95 + i * 370, 80 + 32 * (index - i))
+        else:
+            w, h = get_font_size(font, equip)
+            draw.text(
+                (190 + i * 383 - w, 95 + 32 * (index - i) - h), equip, font_color, font
+            )
+
+    for index, (equip_id, level) in enumerate(info["ex_equip"]):
+        ex_icon = await get_ex_equipment_img(equip_id, 118)
+        im.paste(ex_icon, (43 + 198 * index, 542), mask=ex_icon)
+        if equip_id:
+            await draw_ex_equip_star(
+                im,
+                get_ex_equip_max_star(equip_id),
+                level,
+                16,
+                40 + 200 * index,
+                630,
+            )
+
+    return im
+
 
 async def general_img(all_info):
-    IMAGE_WIDTH = 330
-    IMAGE_HEIGHT = 400
     num = len(all_info)
-    IMAGE_COLUMN = int(num**(1/2))
-    IMAGE_ROW = num//IMAGE_COLUMN + 1 if num % IMAGE_COLUMN != 0 else num//IMAGE_COLUMN
-    base = Image.new('RGB', (IMAGE_COLUMN * IMAGE_WIDTH, IMAGE_ROW * IMAGE_HEIGHT),(0,0,0))
-    font = ImageFont.truetype(font_cn_path,12)
-    icon_size = 95
-    font_black = (77, 76, 81, 255)
-    img_list = []
-    for info in all_info:
-        image = await draw_img(info,font,icon_size,font_black)
-        img_list.append(image)
-    i = 0
-    for y in range(1, IMAGE_ROW + 1):
-        for x in range(1, IMAGE_COLUMN + 1):
-            try:
-                from_image = img_list[i]
-            except:
-                return base
-            base.paste(from_image, ((x - 1) * IMAGE_WIDTH, (y - 1) * IMAGE_HEIGHT))
-            i += 1
+    IMAGE_COLUMN = math.isqrt(num)
+
+    IMAGE_ROW = math.ceil(num / IMAGE_COLUMN)
+    base = Image.new(
+        "RGB", (IMAGE_COLUMN * IMAGE_WIDTH, IMAGE_ROW * IMAGE_HEIGHT), (0, 0, 0)
+    )
+    font = ImageFont.truetype(font_cn_path, 20)
+    img_list = [await draw_img(info, font, 155, (0, 0, 0, 255)) for info in all_info]
+    for i, (y, x) in enumerate(
+        itertools.product(range(1, IMAGE_ROW + 1), range(1, IMAGE_COLUMN + 1))
+    ):
+        if i >= num:
+            break
+
+        from_image = img_list[i]
+        base.paste(from_image, ((x - 1) * IMAGE_WIDTH, (y - 1) * IMAGE_HEIGHT))
     return base
-        
-
-
-
-        
-
-
-        
-
